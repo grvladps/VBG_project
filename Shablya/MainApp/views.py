6e7e8c3094bd7325aba132ceb80d5af1fa0b4cfd
Rosesgold <aaronmercy@gmail.com>
2023-10-10 21:52:20 +0300

    commit #2

--- Shablya/MainApp/views.py
+++ Shablya/MainApp/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index_page(request):
     return render(request, "index.html")
+
+
+def cabinet_page(request):
+    return render(request, "cabinet.html")
--- Shablya/Shablya/urls.py
+++ Shablya/Shablya/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from MainApp.views import index_page
+from MainApp.views import index_page, cabinet_page
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_page)
+    path('', index_page),
+    path('cabinet/', cabinet_page, name='cabinet')
 ]
